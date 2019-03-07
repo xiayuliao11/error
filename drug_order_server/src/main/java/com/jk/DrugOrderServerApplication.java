2aee20e6e6c7efd83255040f96128c929925b494
xiayuliao11 <li@112233>
2019-03-07 11:51:41 +0800

    第一次提交

---
+++ drug_order_server/src/main/java/com/jk/DrugOrderServerApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugOrderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugOrderServerApplication.class, args);
+    }
+
+}
---
+++ drug_order_service_api/src/main/java/com/jk/DrugOrderServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugOrderServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugOrderServiceApiApplication.class, args);
+    }
+
+}
