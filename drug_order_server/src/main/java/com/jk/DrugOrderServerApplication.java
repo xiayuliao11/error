cc448dbefc0c1acf08e74b26b9bf54cc63d9b11e
xiayuliao11 <li@112233>
2019-03-07 15:19:01 +0800

    提交运行

--- drug_order_server/src/main/java/com/jk/DrugOrderServerApplication.java
+++ drug_order_server/src/main/java/com/jk/DrugOrderServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@MapperScan("com.jk.mapper")
+
 @SpringBootApplication
 @EnableEurekaClient
 public class DrugOrderServerApplication {
